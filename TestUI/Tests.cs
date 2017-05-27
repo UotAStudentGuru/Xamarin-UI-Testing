f7e924a74771b17167e82f3680b43c5d71d8660c
georgemakrakis <georgemakrakis88@gmail.com>
2017-05-27 20:21:41 +0300

    Project Commit

---
+++ TestUI/Tests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using Xamarin.UITest;
+
+namespace TestUI
+{
+    [TestFixture(Platform.Android)]
+    //[TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void AppLaunches()
+        {
+            app.Screenshot("First screen.");
+        }
+        [Test]
+        public void check()
+        {
+            app.EnterText("EntryId","@.");
+            app.Tap("ButtonID");
+            var appRes = app.Query("LabelID").First(result=>result.Text=="Confirmed");
+            Assert.IsTrue(appRes != null, "Not right");
+        }
+    }
+}
+
---
+++ TestingXamarin/TestingXamarin/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Xamarin.Forms;
+
+namespace TestingXamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Clicked(object sender, System.EventArgs e)
+        {
+            if(entry.Text.Contains("@") && entry.Text.Contains("."))
+            {
+                Label.Text = "Contains OK";
+            }
+        }
+    }
+}
